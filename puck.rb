a997bfb6fe71326ff1f8766a5a7d44dfbfb2e4ac
Carson Jones <carsoncjones@gmail.com>
2026-01-01 15:37:02 -0600

    update

--- puck.rb
+++ puck.rb
@@ -1,8 +1,8 @@
 class Puck < Formula
   desc "NHL Terminal UI for real-time stats and game tracking"
-  homepage "https://github.com/jones/nhl-tui"
-  url "https://registry.npmjs.org/puck/-/puck-0.1.0.tgz"
-  sha256 "6fc7d276a10b4196c85dc4ab71bc4e98e21927b06ac44b5884d44719fdf5d174"
+  homepage "https://github.com/carsonjones/puck"
+  url "https://registry.npmjs.org/@jonze/puck/-/puck-0.1.0.tgz"
+  sha256 "4a9f145ecb02db5cd81296f05161c0c42f1471cecf90372dfc0a2c41d7ec977e"
   license "MIT"
 
   depends_on "bun"
